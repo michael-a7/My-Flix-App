ddaec1625321ab9863384e1ccddd7b42651daff5
Michael Albo <michael.albo1@yahoo.com>
2021-03-23 16:35:58 -0700

    change styling

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components/macro';
 export const InnerStyle = styled.div`
-align-items: center;
-justify-content: space-between;
-max-width:1100px;
-background-color:red;
-display:flex;
+display: flex;
+justify-content: center;
+flex-direction: ${({direction}) =>direction};
+border: 5px black solid;
+margin: auto;
 `
 ;
