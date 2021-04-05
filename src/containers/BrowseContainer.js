1484a052e3c852c5b4c2f9862f104c6c1fcc5d9e
Michael Albo <michael.albo1@yahoo.com>
2021-04-05 14:08:55 -0700

    browse

---
+++ src/containers/BrowseContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function BrowseContainer({ slides }) {
+  return <p> Text </p>;
+}
+
+export default BrowseContainer;
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,8 +1,13 @@
 import React from "react";
+import BrowseContainer from "../containers/BrowseContainer";
 import { useContent } from "../hooks";
+import selectionMap from "../utils/selection-map";
 
 export default function Browse() {
   const { series } = useContent("series");
   const { films } = useContent("films");
-  return <p>Browsin</p>;
+
+  const slides = selectionMap({ series, films });
+  console.log(slides);
+  return <BrowseContainer slides={slides} />;
 }
