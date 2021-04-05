799a4726d305f0f6969aa36c07b14cc2060986bd
Michael Albo <michael.albo1@yahoo.com>
2021-04-05 15:30:44 -0700

    begin signin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,65 +1,11 @@
 import React, { useState, useContext } from "react";
-import { FirebaseContext } from "../context/firebase";
-import { useHistory } from "react-router-dom";
-import HeaderContainer from "../containers/HeaderContainer";
 import { FooterContainer } from "../containers/FooterContainer";
-import { Form } from "../components";
-import * as ROUTES from "../constants/routes";
-import { SIGN_IN, SIGN_UP } from "../components/links/routes";
-export default function Signin() {
-  const { firebase } = useContext(FirebaseContext);
-  const [emailAddress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
-  const history = useHistory();
-  const [error, setError] = useState();
-  const isInvalid = password === null || emailAddress === null;
-  const handleSignin = (event) => {
-    event.preventDefault();
+import HeaderContainer from "../containers/HeaderContainer";
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(emailAddress, password)
-      .then(() => {
-        history.push(ROUTES.BROWSE);
-      })
-      .catch((error) => {
-        setEmailAddress();
-        setPassword();
-        setError(error.message);
-      });
-  };
+export default function Signin() {
   return (
     <>
-      <HeaderContainer>
-        <Form>
-          <Form.Title>Sign In</Form.Title>
-          {error && <Form.Error>{error}</Form.Error>}
-          <Form.Base onSubmit={handleSignin} method="POST">
-            <Form.Input
-              placeholder="Email address"
-              type="text"
-              value={emailAddress}
-              onchange={({ target }) => setEmailAddress(target.value)}
-            />
-            <Form.Input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onchange={({ target }) => setPassword(target.value)}
-            />
-            <Form.Submit disabled={isInvalid} type="submit">
-              Sign In
-            </Form.Submit>
-          </Form.Base>
-          <Form.Text>
-            New to Netflix? <Form.Link to={SIGN_UP}>Sign up now.</Form.Link>
-          </Form.Text>
-          <Form.SmallText>
-            This page is protected by Google reCAPTCHA to ensure you're not a
-            bot. Learn more.
-          </Form.SmallText>
-        </Form>
-      </HeaderContainer>
+      <HeaderContainer>Hi</HeaderContainer>;
       <FooterContainer />
     </>
   );
