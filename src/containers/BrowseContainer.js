90f4fc931910f9fa39ae3b482546c8b32be4f513
Michael Albo <michael.albo1@yahoo.com>
2021-04-09 14:34:54 -0700

    profile

--- src/containers/BrowseContainer.js
+++ src/containers/BrowseContainer.js
@@ -1,11 +1,19 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import SelectProfileContainer from "./ProfileContainer";
 import { FirebaseContext } from "../context/firebase";
 function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    console.log("profile", profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
   return <SelectProfileContainer user={user} setProfile={setProfile} />;
 }
 
